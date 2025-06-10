d0935f2f395b614fa71d364c0a0e81acd1f40a07
Jen Kelly <jen.kelly@powerhrg.com>
2025-06-10 17:07:29 -0400

    Pass github and linkedin url from About to Links component

--- src/components/App.js
+++ src/components/App.js
@@ -2,19 +2,15 @@ import React from "react"
 import NavBar from "./NavBar"
 import Home from "./Home"
 import About from "./About"
-// import Links from "./Links"
 
 // pass this data down as props to the child component(s) that need it!
 import user from "../data/user"
-
 function App() {
-  console.log(user)
   return (
     <div>
       <NavBar />
       <Home color={"firebrick"} name={user.name} city={user.city} />
       <About
-        isBio={true}
         bio={user.bio}
         github={user.links.github}
         linkedin={user.links.linkedin}
--- src/components/Links.js
+++ src/components/Links.js
@@ -1,12 +1,11 @@
 import React from "react"
 
 function Links(props) {
-  console.log(props)
   return (
     <div id="links">
       <h3>Links</h3>
-      <a href={props.links}></a>
-      <a href={props.links}></a>
+      <a href={props.github}>{props.github}</a>
+      <a href={props.linkedin}>{props.linkedin}</a>
     </div>
   )
 }
